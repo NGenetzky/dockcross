93cfc02a601d1a7c33d91de29edd9993e32f67f0
Nathan Genetzky <nathan@genetzky.us>
2020-06-26 00:54:44 -0500

    yocto: Switch to installing bitbake with poky via kas

--- imagefiles/build-and-install-bitbake.sh
+++ imagefiles/build-and-install-bitbake.sh
@@ -7,31 +7,28 @@ if ! command -v git &> /dev/null; then
 	exit 1
 fi
 
-cd /opt/
-
-git clone "https://github.com/NGenetzky/bitbake.bblayer" /opt/bitbake/
+mkdir -p \
+    /opt/bitbake/build/conf/ \
+    /opt/bitbake/build/layers/
 
 cd /opt/bitbake/
-git checkout -B build 'ca33d32f887b54eeb8ff70f7e660510fedb56197' ;
-git submodule update --init bitbake
 
-mkdir -p '/opt/bitbake/build/conf'
-
-cat << EOF >> /opt/bitbake/build/conf/kas.yml
+cat << EOF >> build/conf/kas.yml
 header:
   version: 8
 env:
-  SHELL: bash # NOTE: This isn't **really** needed, but I like bash.
+  SHELL: /bin/bash # NOTE: This isn't **really** needed, but I like bash.
 target: world
 repos:
-  bblayer:
-    url: /opt/bitbake/
-    refspec: ca33d32f887b54eeb8ff70f7e660510fedb56197
+  poky:
+    url: "https://git.yoctoproject.org/git/poky"
+    refspec: yocto-2.6.4
+    path: "build/layers/yocto-2.6.4/poky/"
     layers:
-      bb_core:
-      bb_fetch:
-      examples/bitbake-core-fetch/layers/example:
+      meta:
+      meta-poky:
+      meta-yocto-bsp:
 EOF
 
-kas shell /opt/bitbake/build/conf/kas.yml -c 'bitbake-layers show-layers'
+kas shell build/conf/kas.yml -c 'bitbake-layers show-layers'
 
