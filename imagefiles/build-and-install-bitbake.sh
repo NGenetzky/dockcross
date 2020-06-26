c834a1d7cf385289bbc234ca0f790b2ebafce3ac
Nathan Genetzky <nathan@genetzky.us>
2020-06-26 10:06:12 -0500

    yocto: Install bitbake and poky into '/opt/yocto/'

--- imagefiles/build-and-install-bitbake.sh
+++ imagefiles/build-and-install-bitbake.sh
@@ -7,12 +7,14 @@ if ! command -v git &> /dev/null; then
 	exit 1
 fi
 
+YOCTO_ROOT='/opt/yocto'
 mkdir -p \
-    /opt/bitbake/build/conf/ \
-    /opt/bitbake/build/layers/
+    "${YOCTO_ROOT}/build/conf" \
+    "${YOCTO_ROOT}/build/layers"
+cd "${YOCTO_ROOT}"
 
-cd /opt/bitbake/
-
+KAS_POKY_VERSION="2.6.4"
+KAS_POKY_PATH="build/layers/yocto-${KAS_POKY_VERSION}/poky"
 cat << EOF >> build/conf/kas.yml
 header:
   version: 8
@@ -22,13 +24,27 @@ target: world
 repos:
   poky:
     url: "https://git.yoctoproject.org/git/poky"
-    refspec: yocto-2.6.4
-    path: "build/layers/yocto-2.6.4/poky/"
+    refspec: "yocto-${KAS_POKY_VERSION}"
+    path: "${KAS_POKY_PATH}"
     layers:
       meta:
       meta-poky:
       meta-yocto-bsp:
 EOF
 
+# This will fetch and set the 'conf' without performing a real build.
+# Ideally no one is using this build directory, except to use bitbake or poky.
 kas shell build/conf/kas.yml -c 'bitbake-layers show-layers'
 
+# Clean up any unneccessary files
+git -C "${KAS_POKY_PATH}" \
+  clean -fdx
+rm -rf \
+  build/bitbake-cookerdaemon.log \
+  build/bitbake.lock \
+  build/cache/ \
+  build/tmp/
+
+# Use symlinks to provide consistent locations for bitbake and poky.
+ln -fsT "${KAS_POKY_PATH}" poky
+ln -fsT "${KAS_POKY_PATH}/bitbake" bitbake
